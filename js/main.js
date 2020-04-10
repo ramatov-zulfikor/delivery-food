60e1e88b7872a0faa20e562a1b487cc848f1cb67
Zulfikor <ramatovv@gmail.com>
2020-04-10 12:43:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+const closeButton = document.querySelector('.close-button');
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+
+function toggleModal() {
+   modal.classList.toggle('is-open');
+}
+
+new WOW().init();
